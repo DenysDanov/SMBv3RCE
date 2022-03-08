8332300f30e7122bf18c7da55e2510be3fe67bc7
DenysDanov <denisdanov18@gmail.com>
2022-03-08 04:43:09 +0200

    best_commit_1

--- scanner.py
+++ scanner.py
@@ -1,30 +1,74 @@
+from colorama import Fore, Back, Style
 import socket
 import struct
 import sys
 from netaddr import IPNetwork
 
+
 pkt = b'\x00\x00\x00\xc0\xfeSMB@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x1f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00$\x00\x08\x00\x01\x00\x00\x00\x7f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00x\x00\x00\x00\x02\x00\x00\x00\x02\x02\x10\x02"\x02$\x02\x00\x03\x02\x03\x10\x03\x11\x03\x00\x00\x00\x00\x01\x00&\x00\x00\x00\x00\x00\x01\x00 \x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\n\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'
 f = open('./hosts.txt','rt')
 subnet = f.read().split('\n')
+result = []
 
+hello_msg = f'''
+\n\n{" "*32}SMBv3RCE improved scanner by\n\n
+{"*"*94}
+                                
+ @@@/      &@@*                  @@@.   /@@%          (@@@%              (@@@%                            
+ /@@%      @@@.                  @@@.   /@@%               
+ .@@@      @@@                   @@@.   /@@%               
+  @@@     /@@%     (@@* /@@*     @@@.   /@@%    @@@*   @@@.   #@@%  @@@,  @@@.     &@@  @@&.              
+  %@@*    @@@*    @@@    /@@@    @@@.   /@@#   @@@     @@@.   #@@%        @@@.   &@@%    #@@#             
+  /@@&    @@@    ,@@%     @@@    @@@.   /@@,  @@@      @@@.   #@@%        @@@.   @@@.    .@@@             
+   @@@   .@@@    .##/     @@@    @@@.   /@@  @@&       @@@.   #@@%        @@@.   @@@     .@@@             
+   @@@   /@@(         /%  @@@    @@@.   ,%%%@@%        @@@.   #@@%        @@@.   @@@  @@@@@@@             
+   #@@/  %@@.     ,@@@*   @@@    @@@.   /@@@@, %(      @@@.   #@@%        @@@.   @@@                      
+   .@@%  @@@     %@@(     @@@    @@@.   /@@@. &@@(     @@@.   #@@%        @@@.   @@@     .&&%             
+    @@@  @@%     @@@.     @@@    @@@.   /@@&   &@@(    @@@.   #@@%        @@@.   @@@.    .@@@             
+        ,@@*     @@@&     @@@    @@@.   /@@&    @@@(   @@@.   #@@%        @@@.   %@@%    #@@#             
+    /@@@@@@       .@@@@&  @@@    @@@.   /@@&     &@@/  @@@.   #@@%        @@@.     @@@  @@@               
+          
+          
+{"*"*94}
+'''
+print(Fore.GREEN + hello_msg)
+print(Fore.CYAN + f'{len(subnet)} IP got')
 for s_ip in subnet:
-    for ip in IPNetwork(s_ip):
-        print(f"[INFO] {ip} \t| Checking...")
-        sock = socket.socket(socket.AF_INET)
-        sock.settimeout(3)
+    try:
+        for ip in IPNetwork(s_ip):
+            print(Fore.CYAN + f"[INFO]\t{ip} \t| Checking...")
+            sock = socket.socket(socket.AF_INET)
+            sock.settimeout(3)
 
-        try:
-            sock.connect(( str(ip),  445 ))
-        except Exception as e:
-            sock.close()
-            print(f'[ERROR] {ip} \t| {e}')   
-            continue
+            try:
+                sock.connect(( str(ip),  445 ))
+            except Exception as e:
+                print(Fore.RED + f'[ERROR]\t{ip} \t| {e}')  
+                sock.close()
+                continue
 
-        sock.send(pkt)
-        nb, = struct.unpack(">I", sock.recv(4))
-        res = sock.recv(nb)
+            sock.send(pkt)
+            nb, = struct.unpack(">I", sock.recv(4))
+            res = sock.recv(nb)
 
-        if res[68:70] != b"\x11\x03" or res[70:72] != b"\x02\x00":
-            print(f"[INFO] {ip} \t| Not vulnerable.")
-        else:
-            print(f"[SUCCESS] {ip} \t| Vulnerable")
+            if res[68:70] != b"\x11\x03" or res[70:72] != b"\x02\x00":
+                print(Fore.RED + f"[INFO]\t{ip} \t| Not vulnerable.")
+            else:
+                result.append(str(ip))
+                print(Fore.GREEN + f"[SUCCESS]\t{ip} \t| Vulnerable")
+    except KeyboardInterrupt:
+        print(Fore.WHITE + 'Interrupted')
+        break
+    
+    except Exception as e:
+        print(Fore.RED + f'[Error]\t{e}')
+        continue
+
+try:
+    open('result.txt','wt').write("\n".join(result))
+except Exception as e:
+    print(Fore.RED + f'[Error]\tFile output error {e}')
+else:
+    print(Fore.GREEN + f'Successfully saved {len(result)} IP adresses')
+
+print(Fore.RESET)
